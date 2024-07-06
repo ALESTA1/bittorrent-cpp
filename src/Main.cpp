d3a309b951b25432ded4a0e570683380815d8156
ALESTA1 <raaghav2468@gmail.com>
2024-07-06 22:59:04 +0530

    testing 2nd stage

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,48 +8,78 @@
 
 using json = nlohmann::json;
 
-json decode_bencoded_value(const std::string& encoded_value) {
-    if (std::isdigit(encoded_value[0])) {
+json decode_bencoded_value(const std::string &encoded_value)
+{
+    if (encoded_value.size() == 0)
+    {
+        throw std::runtime_error("Invalid encoded value: " + encoded_value);
+    }
+    if (std::isdigit(encoded_value[0]))
+    {
         // Example: "5:hello" -> "hello"
         size_t colon_index = encoded_value.find(':');
-        if (colon_index != std::string::npos) {
+        if (colon_index != std::string::npos)
+        {
             std::string number_string = encoded_value.substr(0, colon_index);
             int64_t number = std::atoll(number_string.c_str());
             std::string str = encoded_value.substr(colon_index + 1, number);
             return json(str);
-        } else {
+        }
+        else
+        {
             throw std::runtime_error("Invalid encoded value: " + encoded_value);
         }
-    } else {
+    }
+    else if (encoded_value[0] == 'i')
+    {
+        std::string res = "";
+        for (int i = 1; i < encoded_value.size(); i++)
+        {
+            if (encoded_value[i] == 'e')
+            {
+                break;
+            }
+            res += encoded_value[i];
+        }
+        return json(std::atoll(res.c_str()));
+    }
+    else
+    {
         throw std::runtime_error("Unhandled encoded value: " + encoded_value);
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    if (argc < 2) {
+    if (argc < 2)
+    {
         std::cerr << "Usage: " << argv[0] << " decode <encoded_value>" << std::endl;
         return 1;
     }
 
     std::string command = argv[1];
 
-    if (command == "decode") {
-        if (argc < 3) {
+    if (command == "decode")
+    {
+        if (argc < 3)
+        {
             std::cerr << "Usage: " << argv[0] << " decode <encoded_value>" << std::endl;
             return 1;
         }
         // You can use print statements as follows for debugging, they'll be visible when running tests.
-        //std::cout << "Logs from your program will appear here!" << std::endl;
+        // std::cout << "Logs from your program will appear here!" << std::endl;
 
-        //Uncomment this block to pass the first stage
+        // Uncomment this block to pass the first stage
         std::string encoded_value = argv[2];
         json decoded_value = decode_bencoded_value(encoded_value);
         std::cout << decoded_value.dump() << std::endl;
-    } else {
+    }
+    else
+    {
         std::cerr << "unknown command: " << command << std::endl;
         return 1;
     }
