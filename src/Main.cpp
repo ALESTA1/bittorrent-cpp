47fbad1b56f325e1be57226a42ac4e3e94323a5a
ALESTA1 <raaghav2468@gmail.com>
2024-07-07 18:32:48 +0530

    hash

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,9 +4,10 @@
 #include <cctype>
 #include <cstdlib>
 #include <fstream>
+#include <sstream>
 using namespace std;
 #include "lib/nlohmann/json.hpp"
-#include <openssl/sha.h>
+
 using json = nlohmann::json;
 json decode_bencoded_value(const std::string &encoded_value, int &id);
 json decodeInteger(string encoded_value, int &id)
@@ -119,16 +120,70 @@ string jsonToBencode(json value)
     }
     return res;
 }
-std::string sha1(const std::string &inp)
-{
-    unsigned char hash[SHA_DIGEST_LENGTH];
-    SHA1(reinterpret_cast<const unsigned char *>(inp.c_str()), inp.size(), hash);
-    std::stringstream ss;
-    for (unsigned char i : hash)
-    {
-        ss << std::hex << std::setw(2) << std::setfill('0') << (int)i;
+std::string sha1(const std::string& input) {
+    uint32_t h0 = 0x67452301;
+    uint32_t h1 = 0xEFCDAB89;
+    uint32_t h2 = 0x98BADCFE;
+    uint32_t h3 = 0x10325476;
+    uint32_t h4 = 0xC3D2E1F0;
+    uint64_t bit_length = input.size() * 8;
+    std::vector<uint8_t> data(input.begin(), input.end());
+    data.push_back(0x80);
+    while (data.size() % 64 != 56) {
+        data.push_back(0);
     }
-    return ss.str();
+    for (int i = 7; i >= 0; --i) {
+        data.push_back(bit_length >> (i * 8));
+    }
+    for (std::size_t i = 0; i < data.size(); i += 64) {
+        uint32_t w[80];
+        for (int j = 0; j < 16; ++j) {
+            w[j] = (data[i + j * 4] << 24) | (data[i + j * 4 + 1] << 16) | (data[i + j * 4 + 2] << 8) | data[i + j * 4 + 3];
+        }
+        for (int j = 16; j < 80; ++j) {
+            w[j] = (w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16]);
+            w[j] = (w[j] << 1) | (w[j] >> 31);
+        }
+        uint32_t a = h0;
+        uint32_t b = h1;
+        uint32_t c = h2;
+        uint32_t d = h3;
+        uint32_t e = h4;
+        for (int j = 0; j < 80; ++j) {
+            uint32_t f, k;
+            if (j < 20) {
+                f = (b & c) | ((~b) & d);
+                k = 0x5A827999;
+            } else if (j < 40) {
+                f = b ^ c ^ d;
+                k = 0x6ED9EBA1;
+            } else if (j < 60) {
+                f = (b & c) | (b & d) | (c & d);
+                k = 0x8F1BBCDC;
+            } else {
+                f = b ^ c ^ d;
+                k = 0xCA62C1D6;
+            }
+            uint32_t temp = ((a << 5) | (a >> 27)) + f + e + k + w[j];
+            e = d;
+            d = c;
+            c = (b << 30) | (b >> 2);
+            b = a;
+            a = temp;
+        }
+        h0 += a;
+        h1 += b;
+        h2 += c;
+        h3 += d;
+        h4 += e;
+    }
+    std::ostringstream oss;
+    oss << std::hex << std::setfill('0') << std::setw(8) << h0
+        << std::setw(8) << h1
+        << std::setw(8) << h2
+        << std::setw(8) << h3
+        << std::setw(8) << h4;
+    return oss.str();
 }
 int main(int argc, char *argv[])
 {
